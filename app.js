c0accefa7a230c0ec092807c808645230aaddf20
jianxian <36605259+mengxianbo@users.noreply.github.com>
2023-12-06 16:28:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
-const port= process.env.PORT||3000;
+const uuid= (process.env.UUID||'4ed95035-bb58-4269-b92c-3f36c8d55e2c').replace(/-/g, "");
+const port= process.env.PORT||443;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
 wss.on('connection', ws=>{
